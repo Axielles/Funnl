2074a6a2eedf2f32caf593e96e23dca25681b15d
Mayeul Lefebvre <mayeul.lefebvre@gmail.com>
2018-11-22 09:24:27 +0100

    création des composants sign in/up lost password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarrousselComponent } from './carroussel/carroussel.component';
 import { LateralComponent } from './lateral/lateral.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LostPasswordComponent } from './lost-password/lost-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarrousselComponent,
-    LateralComponent
+    LateralComponent,
+    SignInComponent,
+    SignUpComponent,
+    LostPasswordComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [NgbCarouselConfig],
   bootstrap: [AppComponent]
