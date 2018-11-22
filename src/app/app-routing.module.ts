4466ba885598b1093761b80111cc11ac88259efd
Saida Zghal <zghal.saida@orange.fr>
2018-11-22 12:19:40 +0100

    création component erreur404 + page HTML

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LostPasswordComponent } from './lost-password/lost-password.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { Erreur404Component } from './erreur404/erreur404.component';
 
 const routes: Routes = [
   { path: 'signInForm', component: SignInComponent },
   { path: 'signUpForm', component: SignUpComponent },
   { path: 'lostPassword', component: LostPasswordComponent },
   { path: 'homePage', component: HomePageComponent },
-  { path: '', component: SignInComponent }
+  { path: '', component: SignInComponent },
+  { path: 'not-found', component: Erreur404Component },
+  { path: '**', redirectTo: '/not-found' }
+
 ];
 
 @NgModule({
