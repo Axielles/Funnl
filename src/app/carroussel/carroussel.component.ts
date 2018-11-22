2f5c87d098e6f0fe6707fbb790d0ed0e519b83de
Axielles <awacks@gmail.com>
2018-11-22 12:24:16 +0100

    refacto footer

--- src/app/carroussel/carroussel.component.ts
+++ src/app/carroussel/carroussel.component.ts
@@ -9,7 +9,7 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 export class CarrousselComponent {
 
-  images = [1, 2, 3].map(() => `https://picsum.photos/1440/300?random&t=${Math.random()}`);
+  images = [1, 2, 3].map(() => `https://picsum.photos/1440/430?random&t=${Math.random()}`);
 
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
